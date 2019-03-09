f53fd30cae2769b9e2c276602de8cecd1aaea4bf
Elderick <erickoliveiraah@gmail.com>
2019-03-09 13:39:04 -0300

    Tela de login, signup1 e sign2 finalizadas

---
+++ app/src/main/java/com/example/lebre/signup_personal_info.java
@@ -0,0 +1,25 @@
+package com.example.lebre;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class signup_personal_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_personal_info);
+
+
+
+
+    }
+}
