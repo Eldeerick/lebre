b50e3c6cdeb07e1e637630cec4b9c7a0b2af53e0
Elderick <erickoliveiraah@gmail.com>
2019-03-09 00:19:45 -0300

    Tela de login criada, alterações ainda nescessárias

--- app/src/main/java/com/example/lebre/MainActivity.java
+++ app/src/main/java/com/example/lebre/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.lebre;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button botao_recuperar_senha = (Button) findViewById(R.id.button2);
+        botao_recuperar_senha.setPaintFlags(botao_recuperar_senha.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
 }
