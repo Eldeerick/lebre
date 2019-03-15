a6f7fdf4e7b40f52e7e8ee255828a4784367dd36
Elderick <erickoliveiraah@gmail.com>
2019-03-14 22:34:56 -0300

    Tela de cadastro ajustada, função cadastro grava no arquivo com sucesso, tratamento de erro no email utilizando expressões regulares, ajuste da tela quando o teclado subir adicionado.

--- app/src/main/java/com/example/lebre/signup.java
+++ app/src/main/java/com/example/lebre/signup.java
@@ -1,8 +1,10 @@
 package com.example.lebre;
 
+import android.content.Intent;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,12 +20,12 @@ public class signup extends AppCompatActivity {
 
     private TextInputLayout email;
     private TextInputLayout senha;
-    private TextInputLayout confirmSenha ;
-    private TextInputLayout nomeCompleto ;
-    private TextInputLayout cpf ;
+    private TextInputLayout confirmSenha;
+    private TextInputLayout nomeCompleto;
+    private TextInputLayout cpf;
     private TextInputLayout telefone;
-    private TextInputLayout estado ;
-    private TextInputLayout cidade ;
+    private TextInputLayout estado;
+    private TextInputLayout cidade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,39 +43,48 @@ public class signup extends AppCompatActivity {
 
     }
 
-    public boolean validaEmail(){
+    public boolean validaEmail() {
         String email_content = email.getEditText().getText().toString().trim();
         Gerenciamento_Arquivo.LerUsuarioNoSD(signup.this, usuariosList);
         Usuario verificaEmail = Gerenciamento_Arquivo.VerificaUsuarioArrayList(usuariosList, email_content);
 
-        if(verificaEmail != null){
+        if (verificaEmail != null) {
             email.setError("O email já está cadastrado.");
+            email.requestFocus();
             return false;
-        }else if(email_content.isEmpty()){
+        } else if (email_content.isEmpty()) {
             email.setError("O campo não pode estar vazio.");
+            email.requestFocus();
             return false;
-        }else{
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email_content).matches()) {
+            email.setError("O email não é válido.");
+            email.requestFocus();
+            return false;
+        } else {
             email.setError(null);
             novoUsuario.setEmail(email_content);
             return true;
         }
     }
 
-    public boolean validaSenha(){
+    public boolean validaSenha() {
         String senha_content = senha.getEditText().getText().toString().trim();
         String confirm_senha_content = confirmSenha.getEditText().getText().toString().trim();
 
-        if(senha_content.isEmpty()) {
+        if (senha_content.isEmpty()) {
             senha.setError("Você precisa definir uma senha.");
+            senha.requestFocus();
             return false;
-        }else if(senha_content.length() < 5){
+        } else if (senha_content.length() < 5) {
             senha.setError("A senha precisa ter no mínimo 6 caracteres.");
+            senha.requestFocus();
             return false;
-        }else if(!senha_content.equals(confirm_senha_content)){
+        } else if (!senha_content.equals(confirm_senha_content)) {
             senha.setError("As senhas precisam ser iguais.");
+            senha.requestFocus();
             confirmSenha.setError("As senhas precisam ser iguais.");
             return false;
-        }else{
+        } else {
             senha.setError(null);
             confirmSenha.setError(null);
             novoUsuario.setSenha(senha_content);
@@ -82,80 +93,87 @@ public class signup extends AppCompatActivity {
 
     }
 
-    public boolean validaNomeCompleto(){
+    public boolean validaNomeCompleto() {
         String nomeCompleto_content = nomeCompleto.getEditText().getText().toString().trim();
 
-        if(nomeCompleto_content.isEmpty()){
+        if (nomeCompleto_content.isEmpty()) {
             nomeCompleto.setError("O campo não pode estar vazio.");
+            nomeCompleto.requestFocus();
             return false;
-        }else{
+        } else {
             nomeCompleto.setError(null);
             novoUsuario.setNomeCompleto(nomeCompleto_content);
             return true;
         }
     }
 
-    public boolean validaCPF(){
+    public boolean validaCPF() {
         String cpf_content = cpf.getEditText().getText().toString().trim();
 
-        if(cpf_content.isEmpty()){
+        if (cpf_content.isEmpty()) {
             cpf.setError("O campo não pode estar vazio.");
+            cpf.requestFocus();
             return false;
-        }else if(cpf_content.length() < 10) {
+        } else if (cpf_content.length() < 10) {
             cpf.setError("CPF inválido.");
+            cpf.requestFocus();
             return false;
-        }else{
+        } else {
             cpf.setError(null);
             novoUsuario.setCpf(cpf_content);
             return true;
         }
     }
 
-    public boolean validaTelefone(){
+    public boolean validaTelefone() {
         String telefone_content = telefone.getEditText().getText().toString().trim();
 
-        if(telefone_content.isEmpty()){
+        if (telefone_content.isEmpty()) {
             telefone.setError("O campo não pode estar vazio.");
+            telefone.requestFocus();
             return false;
-        }else{
+        } else {
             telefone.setError(null);
             novoUsuario.setTelefone(telefone_content);
             return true;
         }
     }
 
-    public boolean validaEstado(){
+    public boolean validaEstado() {
         String estado_content = estado.getEditText().getText().toString().trim();
 
-        if(estado_content.isEmpty()){
+        if (estado_content.isEmpty()) {
             estado.setError("O campo não pode estar vazio.");
+            telefone.requestFocus();
             return false;
-        }else{
+        } else {
             estado.setError(null);
             novoUsuario.setEstado(estado_content);
             return true;
         }
     }
 
-    public boolean validaCidade(){
+    public boolean validaCidade() {
         String cidade_content = cidade.getEditText().getText().toString().trim();
 
-        if(cidade_content.isEmpty()){
+        if (cidade_content.isEmpty()) {
             estado.setError("O campo não pode estar vazio.");
+            estado.requestFocus();
             return false;
-        }else{
+        } else {
             estado.setError(null);
             novoUsuario.setCidade(cidade_content);
             return true;
         }
     }
 
-    public void Finalizar(View v){
-        if(!validaEmail() | !validaSenha() | !validaNomeCompleto() | !validaCPF() | !validaTelefone() | !validaEstado() | !validaCidade())
+    public void Finalizar(View v) {
+        if (!validaEmail() | !validaSenha() | !validaNomeCompleto() | !validaCPF() | !validaTelefone() | !validaEstado() | !validaCidade())
             return;
 
-        String content = novoUsuario.getAllInfo();
+        Gerenciamento_Arquivo.gravarUsuarioNoSD(signup.this, novoUsuario.getAllInfo());
+        Intent intent = new Intent(signup.this, login.class);
+        startActivity(intent);
 
-        Toast.makeText(this, content, Toast.LENGTH_SHORT).show();
     }
 }
